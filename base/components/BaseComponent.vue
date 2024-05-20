27c353ca155bdc59a5ef33e28d9b8d6d8fd9d181
Jordan Pisani <pisanij@hillsboroughcounty.org>
2024-05-20 15:04:58 -0400

    add nuxtui card

--- base/components/BaseComponent.vue
+++ base/components/BaseComponent.vue
@@ -1,4 +1,16 @@
 <template>
   <h1>Extending Components is Fun!</h1>
   <button href="#">test button</button>
+
+  <UCard>
+    <template #header>
+      <Placeholder class="h-8" />
+    </template>
+
+    <Placeholder class="h-32" />
+
+    <template #footer>
+      <Placeholder class="h-8" />
+    </template>
+  </UCard>
 </template>
